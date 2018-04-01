0670eb06c955b3d8c3fde5e85fa7e3220ce6784e
hetao <ht@GeekBeandeMacBook-Air-2.local>
2018-04-01 10:19:18 +0800

    [新增] 增加flyio，做http请求

---
+++ src/api/api.js
@@ -0,0 +1,41 @@
+import Fly from 'flyio/dist/npm/hap'
+import fetch from '@system.fetch'
+
+const fly = new Fly(fetch)
+
+fly.config.baseURL = 'http://gank.io/api'
+
+//添加请求拦截器
+fly.interceptors.request.use(request => {
+  //给所有请求添加自定义header
+  request.headers['X-Tag'] = 'flyio'
+  //打印出请求体
+  console.log(request.body)
+  //终止请求
+  //var err=new Error("xxx")
+  //err.request=request
+  //return Promise.reject(new Error(""))
+
+  //可以显式返回request, 也可以不返回，没有返回值时拦截器中默认返回request
+  return request
+})
+
+//添加响应拦截器，响应拦截器会在then/catch处理之前执行
+fly.interceptors.response.use(
+  response => {
+    //只将请求结果的data字段返回
+    return response.data
+  },
+  err => {
+    //发生网络错误后会走到这里
+    //return Promise.resolve("ssss")
+  }
+)
+
+const api = {
+  getData(params) {
+    return fly.get('/data/' + params)
+  }
+}
+
+export default api
